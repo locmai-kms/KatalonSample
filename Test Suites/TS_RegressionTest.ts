3f4e0ef4322f1c6e10df21a1af50afb15745be20
locmai-kms <locmai@kms-technology.com>
2019-07-30 00:04:07 +0700

    add tc

--- Test Suites/TS_RegressionTest.ts
+++ Test Suites/TS_RegressionTest.ts
@@ -10,4 +10,16 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>bdd95bf7-32db-41cb-811e-574175e446fd</testSuiteGuid>
+   <testCaseLink>
+      <guid>41511b6f-4ddf-44fd-aaad-bd091fffbb37</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC01_LoginValid</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>722bb0cb-d7b6-4cce-bd88-12be73c9a27b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC02_LoginInvalid</testCaseId>
+   </testCaseLink>
 </TestSuiteEntity>
